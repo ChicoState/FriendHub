f71fa0c803a1a28d843c188a92b2fcc988af0196
Grant Meyer <gmeyer1@csuchico.edu>
2023-09-22 20:35:09 -0700

    added user model and test

--- application/models.py
+++ application/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+class FriendHubUser(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
 
-# Create your models here.
-
-#Grant Meyer
+    def __str__(self) -> str:
+        return self.username
+    
