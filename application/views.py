498db435e69ef775300bbf6cb6d522417fb16a22
Numan Khan <101899800+numan-7@users.noreply.github.com>
2023-09-23 12:44:21 -0700

    Created Join/Login Forms

--- application/views.py
+++ application/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from application.forms import JoinForm, LoginForm
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home.html')
 # Create your views here.
+
+def join(request):
+    if (request.method == "POST"):
+        jform = JoinForm(request.POST)
+        if (jform.is_valid()):
+            # save form data to DB
+            user = jform.save()
+            # encrypt the password
+            user.set_password(user.password)
+            # save encrypted password to DB
+            user.save()
+            # success
+            return redirect("/")
+        else:
+            # invalid form
+            page_data = { "join_form": jform }
+            return render(request, 'join.html', page_data)
+    else:
+        jform = JoinForm()
+        page_data = { "join_form": jform }
+        return render(request, 'join.html', page_data)
+
+def user_login(request):
+    if (request.method == 'POST'):
+        lform = LoginForm(request.POST)
+        if lform.is_valid():
+            username = lform.cleaned_data["username"]
+            password = lform.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request,user)
+                    return redirect("/")
+                else:
+                    return HttpResponse("This account is not active.")
+            else:
+                return render(request, 'login.html', {"login_form": LoginForm, "correct": False})
+    else:
+        return render(request, 'login.html', {"login_form": LoginForm, "correct": True})
+
+@login_required(login_url='/login/')
+def user_logout(request):
+    logout(request)
+    return redirect("/")
