60952493acdb12bb3355d5eb45143a6a74719172
Shadowlief <shadowfire510@gmail.com>
2023-09-30 16:21:06 -0700

    IN THEORY adding a simple ass Friend List page

--- FriendHub/urls.py
+++ FriendHub/urls.py
@@ -19,7 +19,7 @@ from application import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", app_views.home),
+    path("friendList", app_views.home),
     path("login/", app_views.user_login),
     path("join/", app_views.join),
     path("logout/", app_views.user_logout),
