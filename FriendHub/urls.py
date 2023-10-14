c6163ec174328e0fc7d68642c7bf42fb1730ec1e
Tony-Ton3 <tongyuzhu1026@gmail.com>
2023-10-14 16:36:55 -0700

    django views test cases

--- FriendHub/urls.py
+++ FriendHub/urls.py
@@ -4,12 +4,12 @@ from application import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("friendList/", app_views.friendList, name='friendList'),
-    path("", app_views.map, name = 'home'),
-    path("login/", app_views.user_login, name = 'login'),
-    path("join/", app_views.join, name = 'join'),
-    path("logout/", app_views.user_logout, name = 'logout'),
-    path("map/", app_views.map, name = 'map'),
+    path('friendList/', app_views.friendList, name='friendList'),
+    path('', app_views.map, name='home'),
+    path('login/', app_views.user_login, name='login'),
+    path('join/', app_views.join, name='join'),
+    path('logout/', app_views.user_logout, name='logout'),
+    path('map/', app_views.map, name='map'),
     path('gMap/', app_views.loadMapAPI, name='gMap'),
     path('sendFriendRequest/', app_views.sendFriendRequest, name='sendFriendRequest'),
     path('acceptFriendRequest/<int:requestId>/', app_views.acceptFriendRequest, name='acceptFriendRequest'),
@@ -18,4 +18,4 @@ urlpatterns = [
     path('removeFriend/<int:friendId>/', app_views.removeFriend, name='removeFriend'),
     path('setDistancePreference/', app_views.setDistancePreference, name='setDistancePreference'),
     path('setColorPreference/', app_views.setColorPreference, name='setColorPreference'),
-]   
+]
