293e66d9a49d98dbb320e05d06f4188215ad5cdf
numan-7 <numankhan165@gmail.com>
2023-10-23 15:53:39 -0700

    Created Tests For Friends Page & Forms

--- application/tests.py
+++ application/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from application.models import UserData, FriendRequest, FriendList
-import json
+from .forms import DistancePreferenceForm, IconPreferenceForm, ColorPreferenceForm
 
 class TestViews(TestCase):
 
@@ -14,20 +14,27 @@ class TestViews(TestCase):
         self.mapUrl = reverse('map')
         self.friendListUrl = reverse('friendList')
         self.homeUrl = reverse('home')
+        self.distanceUrl = reverse('setDistancePreference')
+        self.colorUrl = reverse('setColorPreference')
+        self.iconUrl = reverse('setIconPreference')
 
+        # Create User 1
         self.testUser1 = User.objects.create_user(username='testuser1', password='password')
         self.testUser1Data = UserData.objects.create(
             djangoUser=self.testUser1,
             latitude=1.0,
             longitude=1.0
         )
+        _, _ = FriendList.objects.get_or_create(user= self.testUser1)
 
+        # Create User 2
         self.testUser2 = User.objects.create_user(username='testuser2', password='password')
         self.testUser2Data = UserData.objects.create(
             djangoUser=self.testUser2,
             latitude=2.0,
             longitude=2.0
         )
+        _, _ = FriendList.objects.get_or_create(user= self.testUser2)
 
     # Test Join
     def testJoin(self):
@@ -106,3 +113,92 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(response.context['user'].is_authenticated)
         self.assertTemplateUsed(response, 'login.html')
+
+    # Send Friend Request
+    def testSendFriendReq(self):
+        self.client.login(username='testuser1', password='password')
+        response = self.client.post(reverse('sendFriendRequest'), {'username1': 'testuser2'})
+        self.assertEquals(response.status_code, 302)  # Check for redirect, meaning the request was processed
+        # Check if FriendRequest was created
+        self.assertTrue(FriendRequest.objects.filter(sender=self.testUser1, receiver=self.testUser2).exists())
+
+    # Test Accept Friend Request
+    def testAcceptFriendReq(self):
+        self.client.login(username='testuser1', password='password')
+        friend_request = FriendRequest.objects.create(sender=self.testUser2, receiver=self.testUser1)  # Mock a friend request
+
+        response = self.client.get(reverse('acceptFriendRequest', args=[friend_request.id]))
+        self.assertEquals(response.status_code, 302)  # Check for redirect, meaning the request was processed
+
+        # Check if users are now friends
+        self.assertTrue(self.testUser1.user.friends.filter(pk=self.testUser2.pk).exists())
+        self.assertTrue(self.testUser2.friends.filter(pk=self.testUser1.user.pk).exists())
+
+    # Test Decline Friend Request
+    def testDeclineFriendReq(self):
+        self.client.login(username='testuser1', password='password')
+        friend_request = FriendRequest.objects.create(sender=self.testUser2, receiver=self.testUser1)  # Mock a friend request
+
+        response = self.client.get(reverse('declineFriendRequest', args=[friend_request.id]))
+        self.assertEquals(response.status_code, 302)  # Check for redirect, meaning the request was processed
+
+        # Check that the FriendRequest no longer exists
+        self.assertFalse(FriendRequest.objects.filter(pk=friend_request.pk).exists())
+
+    # Test Cancel Sent Friend Req
+    def testCancelFriendReq(self):
+        self.client.login(username='testuser1', password='password')
+        friend_request = FriendRequest.objects.create(sender=self.testUser1, receiver=self.testUser2)  # Mock a friend request
+
+        response = self.client.get(reverse('cancelFriendRequest', args=[friend_request.id]))
+        self.assertEquals(response.status_code, 302)  # Check for redirect, meaning the request was processed
+
+        # Check that the FriendRequest no longer exists
+        self.assertFalse(FriendRequest.objects.filter(pk=friend_request.pk).exists())
+
+    def testRemoveFriend(self):
+        self.client.login(username='testuser1', password='password')
+
+        # Mocking the users being friends initially
+        self.testUser1.user.friends.add(self.testUser2)
+        self.testUser2.friends.add(self.testUser1.user)
+
+        response = self.client.get(reverse('removeFriend', args=[self.testUser2.pk]))
+        self.assertEquals(response.status_code, 302)  # Check for redirect, meaning the request was processed
+
+        # Check that the users are no longer friends
+        self.assertFalse(self.testUser1.user.friends.filter(pk=self.testUser2.pk).exists())
+        self.assertFalse(self.testUser2.friends.filter(pk=self.testUser1.user.pk).exists())        
+
+    def testValidDistanceForm(self):
+        form = DistancePreferenceForm(data={'distance': 2})
+        self.assertTrue(form.is_valid())
+
+    def testValidColorForm(self):
+        form = ColorPreferenceForm(data={'color': 1})
+        self.assertTrue(form.is_valid())
+
+    def testValidIconForm(self):
+        form = IconPreferenceForm(data={'icon': 2})
+        self.assertTrue(form.is_valid())
+
+    def testSetDistanceForm(self):
+        self.client.login(username='testuser1', password='password')
+        response = self.client.post(self.distanceUrl, {'distance': 2})
+        self.testUser1.refresh_from_db()
+        self.assertEqual(self.testUser1.distancePreference, 2)
+        self.assertEqual(response.status_code, 302)  # 302 is the status code for a successful redirect
+
+    def testSetColorForm(self):
+        self.client.login(username='testuser1', password='password')
+        response = self.client.post(self.colorUrl, {'color': 1})
+        self.testUser1.refresh_from_db()
+        self.assertEqual(self.testUser1.colorPreference, 1)
+        self.assertEqual(response.status_code, 302)
+
+    def testSetIconForm(self):
+        self.client.login(username='testuser1', password='password')
+        response = self.client.post(self.iconUrl, {'icon': 2})
+        self.testUser1.refresh_from_db()
+        self.assertEqual(self.testUser1.iconPreference, 2)
+        self.assertEqual(response.status_code, 302)
